cf16e612541e100bde430df06bfd0abaef2f9407
Marius-47 <marius.pujol@students.salle.url.edu>
2026-06-06 22:26:57 +0200

    Implementacio de la funcionalitat feta

--- task/task.c
+++ task/task.c
@@ -419,6 +419,18 @@ void showInProgressTasks(Task tasks[], int total) {
 }
 
 
+//Mostrem les tasques que ja estan completades, que es en aquest cas es mostra el contador de quantes ja s'han completat
+void showCompletedTasksTotal(Task tasks[], int total) {
+    int completedTasks = 0;
+
+    for (int i = 0; i < total; i++) {
+        if (tasks[i].status == COMPLETED) {
+            completedTasks++;
+        }
+    }
+
+    printf("\nCompleted tasks: %d\n", completedTasks);
+}
 
 //Mostar la funcionalitat en general de les tasques
 void showProductionStatus(void) {
@@ -431,4 +443,5 @@ void showProductionStatus(void) {
 
     showPendingTasks(tasks, total);
     showInProgressTasks(tasks, total);
+    showCompletedTasksTotal(tasks, total);
 }
